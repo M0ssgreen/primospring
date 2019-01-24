4520ca1b9ca1e6dfe5cb3643241d14f6e8cf1b3b
François <francois.sam@consultants-solutec.fr>
2019-01-24 12:29:12 +0100

    Creation du projet primo et mise en commentaire de la creation d'user

---
+++ src/main/java/com/solutec/PrimoApplication.java
@@ -0,0 +1,29 @@
+package com.solutec;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.solutec.dao.UserRepository;
+import com.solutec.entities.User;
+
+@SpringBootApplication
+public class PrimoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepos;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PrimoApplication.class, args);
+	}
+
+/**	@Override //ajout de données dans la bdd user
+	public void run(String... args) throws Exception {
+		userRepos.save(new User("nom","jojo@moi.com"));
+		userRepos.save(new User("toto","toto@moi.com"));
+		userRepos.save(new User("lala","lala@moi.com"));
+	}
+*/
+}
+
