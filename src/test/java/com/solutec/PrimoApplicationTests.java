794806a65eddb9081b02ce97da1f96f773cf0407
François <francois.sam@consultants-solutec.fr>
2019-01-24 12:33:01 +0100

    test commit

---
+++ src/test/java/com/solutec/PrimoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.solutec;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrimoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
